624772185d09351df217698e3385dc2beb9b06a6
Martín Ponce <martinp88@gmail.com>
2024-09-13 18:01:30 -0300

    fix projectes layout again

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,9 +4,7 @@
   <main>
     <h1>Proyectos</h1>
     <div class="row">
-      <div class="col-12 col-lg-6 offset-lg-3">
-        <img src="/src/assets/projects.webp" />
-      </div>
+      <img src="/src/assets/projects.webp" />
     </div>
   </main>
 </template>
