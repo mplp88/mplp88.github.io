c7261dd9e57d8ffe10b4004957f23d3a20635570
Martín Ponce <martinp88@gmail.com>
2024-09-13 17:57:33 -0300

    fix projects layout

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -4,7 +4,7 @@
   <main>
     <h1>Proyectos</h1>
     <div class="row">
-      <div class="col-sm-12 col-lg-6 offset-lg-3">
+      <div class="col-12 col-lg-6 offset-lg-3">
         <img src="/src/assets/projects.webp" />
       </div>
     </div>
