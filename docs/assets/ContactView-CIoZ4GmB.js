38ac4a9660dc8da618e940266336f85908df0fd3
Martín Ponce <martinp88@gmail.com>
2024-10-12 10:29:01 -0300

    new release

--- docs/assets/ContactView-CIoZ4GmB.js
+++
@@ -1,1 +0,0 @@
-import{c as s,a as o,F as c,o as l}from"./index-DRoed9Q-.js";const i=(n,t)=>{const e=n.__vccOpts||n;for(const[r,a]of t)e[r]=a;return e},f={};function u(n,t){return l(),s(c,null,[t[0]||(t[0]=o("h1",null,"Contacto",-1)),t[1]||(t[1]=o("div",{class:"alert alert-warning"},"Página en construcción",-1))],64)}const d=i(f,[["render",u]]);export{d as default};
