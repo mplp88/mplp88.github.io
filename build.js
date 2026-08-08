2c6baed4755bb950e27eaa4f1e436648a7537d25
Martín Ponce <martinp88@gmail.com>
2026-08-08 17:10:53 -0300

    update build script

--- build.js
+++ build.js
@@ -1,8 +1,5 @@
 import { rimraf } from 'rimraf'
 import { exec } from 'child_process'
-import { rename, writeFile } from 'fs/promises'
-import { join, dirname } from 'path'
-import { fileURLToPath } from 'url'
 
 // Convertir exec a una función basada en Promesas
 const execAsync = (command) =>
@@ -30,19 +27,6 @@ const main = async () => {
     if (stderr) {
       console.error(stderr)
     }
-
-    // Renombrar la carpeta dist a docs
-    await rename('dist', 'docs')
-    console.log('Carpeta dist renombrada a docs correctamente.')
-
-    // Crear el archivo CNAME
-    const __filename = fileURLToPath(import.meta.url)
-    const __dirname = dirname(__filename)
-    const filePath = join(__dirname, 'docs', 'CNAME')
-    const domain = 'portfolio.martinponce.com.ar'
-
-    await writeFile(filePath, domain)
-    console.log(`Archivo CNAME creado en ${filePath} con el dominio ${domain}.`)
   } catch (error) {
     console.error('Error:', error)
     process.exit(1) // Termina el proceso con un error
